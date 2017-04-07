e6b02eade7c34eec38c966e7f429bbbd2a1a1f0b
tmnhat2001 <tmnhat2001@users.noreply.github.com>
2017-04-07 00:32:15 -0400

    Added parameters and new pattern

--- serial.c
+++ serial.c
@@ -25,12 +25,30 @@ void free2d(int** grid, int w) {
 }
 
 int main(int argc, char* argv[]) {
-	if (argc < 3) puts("Usage: ./serial rows cols");
+	if (argc < 4) {
+		puts("Usage: ./serial rows cols stages [pattern]");
+		puts("Default pattern is glider");
+		puts("Pattern options are:");
+		printf("-glider\n");
+		printf("-lwss\n");
+		return -1;
+	}
 
 	int w = atoi(argv[1]);
 	int h = atoi(argv[2]);
+	int max_stage = atoi(argv[3]);
 
-	if (w < 10 || h < 10) puts("Number of rows and columns must be at least 20");
+	if (w < 10 || h < 10) {
+		puts("Usage: ./serial rows cols stages [pattern]");
+		puts("Number of rows and columns must be at least 10");
+		return -1;
+	}
+
+	if (max_stage < 1) {
+		puts("Usage: ./serial rows cols stages [pattern]");
+		puts("Number of stages must be at least 1");
+		return -1;
+	}
 
 	int** grid = (int**) malloc(sizeof(int*) * w);
 	fill_grid(grid, w, h);
@@ -38,15 +56,38 @@ int main(int argc, char* argv[]) {
 	// temporary grid to store new cells
 	int** tmp = (int**) malloc(sizeof(int*) * w);
 
-	// glider configuration
-	grid[5][5] = 1;
-	grid[6][6] = 1;
-	grid[7][4] = 1;
-	grid[7][5] = 1;
-	grid[7][6] = 1;
-	
+	// initialize pattern
+	if (argc == 4 || strcmp(argv[4], "glider") == 0) {
+		grid[5][5] = 1;
+		grid[6][6] = 1;
+		grid[7][4] = 1;
+		grid[7][5] = 1;
+		grid[7][6] = 1;
+	} else if (strcmp(argv[4], "lwss") == 0) {
+		grid[0][0] = 1;
+		grid[0][3] = 1;
+		grid[1][4] = 1;
+		grid[2][0] = 1;
+		grid[2][4] = 1;
+		grid[3][4] = 1;
+		grid[3][1] = 1;
+		grid[3][2] = 1;
+		grid[3][3] = 1;
+	} else {
+		puts("Invalid pattern");
+		puts("Usage: ./serial rows cols [pattern]");
+		puts("Default pattern is glider");
+		puts("Pattern options are:");
+		printf("-glider\n");
+		printf("-lwss\n");
+
+		free2d(grid, w);
+		free2d(tmp, w);
+		return -1;
+	}
+
 	int k = 0;
-	while(1) {
+	while(k < max_stage) {
 		printf("Time %d\n", k);
 		
 		fill_grid(tmp, w, h);
